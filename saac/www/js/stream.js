e2a2dc5eb729a1b623f6ae3ebcbe0ceb9209aedf
James D McCants <jamesdmccants@gmail.com>
2016-10-15 22:55:56 -0500

    adds pull refresh

--- saac/www/js/stream.js
+++ saac/www/js/stream.js
@@ -20,4 +20,10 @@ angular.module('saac.stream', ['saac.photo', 'saac.start'])
 	.controller('StreamCtrl', function ($scope, $rootScope, Picture, UserService) {
 		$scope.user = UserService.getUser();
 		$scope.photos = Picture.all();
+
+		$scope.doRefresh = function () {
+			$scope.photos = Picture.all();
+
+			$scope.$broadcast('scroll.refreshComplete');
+		}
 	});
